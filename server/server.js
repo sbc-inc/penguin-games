ce2ae2548070d394f8784c308424ad7e729ff08e
Riley <24r.waghorn@thelenham.viat.org.uk>
2025-08-16 19:09:21 +0100

    Start Server Setup

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+
+const port = 3000;
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send({ message: "Server is running." });
+});
+
+app.get("/viewUserInfo", (req, res) => {
+  res.send({
+    user: {
+      id: 285974,
+      name: "ok_overture_ajr",
+      displayName: "ok_overture_ajr",
+      avatar: "https://cdn.penguingames.com/avatars/285974.png",
+
+    },
+    game: {
+        id: 2048569035,
+        name: "Murder Mystery",
+        description: "Explore the mysterious tomb and uncover murders that were never found.",
+        genre: "Action",
+        playedForMin: 30.3
+    }
+  });
+});
+
+app.use((req, res, next) => {
+  res.status(404).send({ error: "Invalid API Endpoint (404)" });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
